4b075e2d60b0d8accd024cb23b4e9e5b7068e677
RomanDark <amaii.sakura@gmail.com>
2018-10-29 09:17:11 +0600

    Update _string.h

--- class.string/_string.h
+++ class.string/_string.h
@@ -17,11 +17,13 @@ public:
 	_string(const char*str);
 	_string(const _string &obj);
 
-
 	int getLength();
+
 	const char* getString();
 	void setString(const char*str);
 	void input();
+	void insert(int position, const char *str);
+	void erase(int position, int number);
 	void clear();
 	void output();
 
